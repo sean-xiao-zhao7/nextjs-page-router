c86787e1d7129d294ec02b040b6a5c63abfa1f0f
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2024-04-04 14:26:44 -0400

    Add slugged news links

--- src/pages/news/[id].tsx
+++ src/pages/news/[id].tsx
@@ -1,7 +1,11 @@
+import { useRouter } from "next/router";
+
 export default function GetNewsById() {
+    const router = useRouter();
+
     return (
         <>
-            <h1>News by Id</h1>
+            <h1>News by {router.query.id}</h1>
         </>
     );
 }
--- src/pages/news/index.tsx
+++ src/pages/news/index.tsx
@@ -1,5 +1,13 @@
+import Link from "next/link";
+
 const NewsPage = () => {
-    return <h1>News page</h1>;
+    return (
+        <>
+            <h1>News page</h1>
+            <Link href="news/news1">News 1</Link>
+            <Link href="news/news2">News 2</Link>
+        </>
+    );
 };
 
 export default NewsPage;
